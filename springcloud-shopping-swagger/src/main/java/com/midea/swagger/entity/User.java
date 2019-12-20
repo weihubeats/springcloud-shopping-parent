3a6a7e7350f7fdb814b36f03d3b7a546c462fa45
EX_ZHENGWK <597931083@qq.com>
2019-12-20 12:02:56 +0800

    修改module

--- springcloud-shopping-swagger/src/main/java/com/midea/swagger/entity/User.java
+++ springcloud-shopping-swagger/src/main/java/com/midea/swagger/entity/User.java
@@ -16,13 +16,9 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "用户实体", description = "User Entity")
 public class User implements Serializable {
     private static final long serialVersionUID = 5057954049311281252L;
-    @ApiModelProperty(value = "主键id", required = true)
     private Integer id;
-    @ApiModelProperty(value = "用户名", required = true)
     private String name;
-    @ApiModelProperty(value = "密码", required = true)
     private String password;
 }
